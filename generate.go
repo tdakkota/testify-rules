8c593bff5f5f8e37c44d956890777eb747fabec0
tdakkota <tanc13@yandex.ru>
2021-11-17 11:18:14 +0300

    chore: move prefer rules to prefer.go

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package gorules
 
-//go:generate go run github.com/tdakkota/testify-rules/cmd/genprefer -output gorules.go
+//go:generate go run github.com/tdakkota/testify-rules/cmd/genprefer -output prefer.go
